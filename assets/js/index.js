03f0cadc987a4dbc1a82b84e29dbe6f22d893859
tanner johnson <tannerjohnson08@gmail.com>
2024-07-12 11:14:49 -0500

    added base code to index.js

---
+++ assets/js/index.js
@@ -0,0 +1,26 @@
+let userGenre = "";
+let userPlatform = "";
+
+
+// need to add the IDs for the formfields
+function populateData() {
+    userGenre = document.getElementById("").value || "all";
+    userPlatform = document.getElementById("").value || "all";
+    let userSort = "release-date";
+
+    let newUrl = "./search.html";
+    newUrl = `${newUrl}?category=${userGenre}&platform=${userPlatform}&sort-by=${userSort}`
+
+    
+}
+
+function toSearchPage(newUrl){
+    window.location.assign(newUrl);
+}
+
+
+//make sure the ".btn" is correct in index.html
+document.querySelector(".btn").addEventListener("click", function(e) {
+    e.preventDefault();
+    populateData();
+})
